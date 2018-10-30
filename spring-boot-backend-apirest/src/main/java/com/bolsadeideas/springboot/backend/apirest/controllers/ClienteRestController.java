2b404a3444b1c1dcfe6a73b13c98cc74f06a8e86
josemar <josemar7@gmail.com>
2018-10-30 10:35:13 +0100

    mi commit!

---
+++ spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -0,0 +1,63 @@
+package com.bolsadeideas.springboot.backend.apirest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+import com.bolsadeideas.springboot.backend.apirest.models.service.IClienteService;
+
+@CrossOrigin(origins= {"http://localhost:4200"})
+@RestController
+@RequestMapping("/api")
+public class ClienteRestController {
+	
+	@Autowired
+	private IClienteService clienteService;
+	
+	@GetMapping("/clientes")
+	public List<Cliente> index() {
+		return this.clienteService.findAll();
+	}
+	
+	@GetMapping("/clientes/{id}")
+	//es el codigo que devuelve por defecto
+//	@ResponseStatus(HttpStatus.OK)
+	public Cliente show(@PathVariable Long id) {
+		return this.clienteService.findById(id);
+	}
+	
+	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente create(@RequestBody Cliente cliente) {
+		return this.clienteService.save(cliente);
+	}
+	
+	@PutMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente cliente2 = clienteService.findById(id);
+		cliente2.setApellido(cliente.getApellido());
+		cliente2.setNombre(cliente.getNombre());
+		cliente2.setEmail(cliente.getEmail());
+		return clienteService.save(cliente2);
+	}
+	
+	@DeleteMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		clienteService.delete(id);
+	}
+
+}
